5e3f5c04535b42ff382ca050922f8396ceba593d
Hope Ranker <hope.ranker@gmail.com>
2019-01-06 13:31:07 -0500

    Remove compromised slack app key

--- config/slack.rb
+++ config/slack.rb
@@ -4,7 +4,7 @@
 
 # Required
 set :slack_subdomain, 'decaturmakers' # if your Slack subdomain is example.slack.com
-set :slack_url, 'https://hooks.slack.com/services/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' # https://my.slack.com/services/new/incoming-webhook
+set :slack_url, 'REDACTED' # https://my.slack.com/services/new/incoming-webhook
 
 # Optional
 set :slack_channel, '#website'
